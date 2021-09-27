283747383374302a22c622b1f1c346b676de162e
Lucas Amaral Cruz Aragão <82917923+AmaralRage@users.noreply.github.com>
2021-09-27 20:13:21 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+var listaFilmes = [
+  "https://pbs.twimg.com/media/Ecf_DrUWkAAnSm9.jpg:large",
+  "https://rootjogos.com/wp-content/uploads/2020/11/demonsouls.jpg",
+  "https://www.ziliongames.com.br/banco_imagens/produtos/p/elden-ring-ps5-pre-venda-janeiro-202217kN.png"
+];
+
+for (var i = 0; i < listaFilmes.length; i++) {
+  document.write("<img src=" + listaFilmes[i] + ">");
+}
